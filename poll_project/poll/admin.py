8b6b175c44b14daa18cb0e56cbf80443d3fe42d2
abdulfarid.atiq@cba.ku.edu.kw <abdulfarid.atiq@cba.ku.edu.kw>
2023-11-08 20:47:25 +0300

    All done!

--- poll_project/poll/admin.py
+++ poll_project/poll/admin.py
@@ -3,29 +3,6 @@ from django.db.models import Count
 from .models import Poll, Option, Response
 
 # Register your models here.
-<<<<<<< Updated upstream
-class OptionInline(admin.TabularInline):
-    model = Option
-    extra = 1
-
-class ResponseInline(admin.TabularInline):
-    model = Response
-    extra = 1
-
-class PollAdmin(admin.ModelAdmin):
-    inlines = [OptionInline, ResponseInline]
-    list_display = ('question', 'active_until', 'status', 'response_count', 'question_and_options')
-    search_fields = ['question']
-    list_filter = ['status']
-
-    def response_count(self, obj):
-        return obj.option_set.aggregate(response_count=Count('response')).get('response_count', 0)
-
-    response_count.short_description = 'Responses'
-
-    def question_and_options(self, obj):
-        options = obj.option_set.all()
-=======
 
 class OptionInline(admin.TabularInline):   #TabularInlineused to display related model objects in a tabular, making it suitable for cases you have a list of related items to display and edit.
     model = Option
@@ -48,7 +25,6 @@ class PollAdmin(admin.ModelAdmin):
  
     def question_and_options(self, obj):
         options = obj.option_set.all()  # obj.obtion_set is a queryset
->>>>>>> Stashed changes
         return ', '.join([option.text for option in options])
     question_and_options.short_description = 'Question and Options'
 
@@ -61,16 +37,8 @@ class ResponseAdmin(admin.ModelAdmin):
         return obj.option.text
     option_text.short_description = 'Option'
 
-<<<<<<< Updated upstream
-    def poll_title(self, obj):
-        return obj.option.poll.title
-    poll_title.short_description = 'Poll'
-
-admin.site.register(Response, ResponseAdmin)
-=======
     def poll_title(self, obj):   #obj is an instance of the model. 
         return obj.option.poll.title
     poll_title.short_description = 'Poll'
 
 admin.site.register(Response, ResponseAdmin)
->>>>>>> Stashed changes
